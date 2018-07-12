68e75f38024f2364788f9aa78c6fb1cf0cb556d1
lixueli <lixueli@qiniu.com>
2018-07-12 17:27:18 +0800

    add anchor.rb 0.1.0

---
+++ anchor.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Anchor < Formula
+  desc "Anchor is commandline tool for vendors to manage charts and app specs in KE App Store."
+  homepage ""
+  url "http://oyh1cogl9.bkt.clouddn.com/anchor-0.1.0.tar.gz"
+  sha256 "da7bec321289d90cbd8f84d82091f31fe4b50ee14d69bc25dfe71a79d5ea1c8d"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # # system "cmake", ".", *std_cmake_args
+    # system "make", "install" # if this fails, try separate make/make install steps
+    bin.install "anchor"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test anchor`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
